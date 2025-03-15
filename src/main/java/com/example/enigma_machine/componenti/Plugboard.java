37a01d04c4fc7f3a2ea319e8c3dc31d85d42f12d
P4gl14 <paglieranipietro@gmail.com>
2025-03-15 16:51:17 +0100

    gestione maiuscole/minuscole

--- src/main/java/com/example/enigma_machine/componenti/Plugboard.java
+++ src/main/java/com/example/enigma_machine/componenti/Plugboard.java
@@ -1,4 +1,30 @@
 package com.example.enigma_machine.componenti;
 
+import java.util.HashMap;
+
 public class Plugboard {
+    private HashMap<Character, Character> scambi;
+
+    public Plugboard() {
+        this.scambi = new HashMap<>();
+    }
+
+    public void aggiungiScambio(Character lettera1, Character lettera2) {
+        lettera1 = Character.toUpperCase(lettera1);
+        lettera2 = Character.toUpperCase(lettera2);
+        this.scambi.put(lettera1, lettera2);
+        this.scambi.put(lettera2, lettera1);
+    }
+
+    public void rimuoviScambio(Character lettera1, Character lettera2) {
+        lettera1 = Character.toUpperCase(lettera1);
+        lettera2 = Character.toUpperCase(lettera2);
+        this.scambi.remove(lettera1);
+        this.scambi.remove(lettera2);
+    }
+
+    public Character scambia (Character lettera) {
+        lettera = Character.toUpperCase(lettera);
+        return this.scambi.getOrDefault(lettera, lettera);
+    }
 }
