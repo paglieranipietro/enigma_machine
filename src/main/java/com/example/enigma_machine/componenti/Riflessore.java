5b1a274ebba278da534935afa43414588c2610ac
P4gl14 <paglieranipietro@gmail.com>
2025-03-15 16:26:28 +0100

    da iniziare

---
+++ src/main/java/com/example/enigma_machine/componenti/Riflessore.java
@@ -0,0 +1,20 @@
+package com.example.enigma_machine.componenti;
+
+import java.util.HashMap;
+
+public class Riflessore {
+    private HashMap<Character, Character> caratteri;
+
+    public Riflessore() {
+        this.caratteri = new HashMap<>();
+        int i = 0;
+        for (Character ch : Configurazioni.getRiflessore()) {
+            this.caratteri.put((char)('A' + i), ch);
+            i++;
+        }
+    }
+
+    public Character codifica(Character lettera) {
+        return this.caratteri.get(lettera);
+    }
+}
