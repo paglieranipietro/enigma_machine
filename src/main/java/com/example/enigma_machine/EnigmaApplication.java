59830d93c0430a39750d464a8b1da2899f401bc9
P4gl14 <paglieranipietro@gmail.com>
2025-03-15 17:34:16 +0100

    funzionamento con 2 TextArea

--- src/main/java/com/example/enigma_machine/EnigmaApplication.java
+++ src/main/java/com/example/enigma_machine/EnigmaApplication.java
@@ -11,7 +11,7 @@ public class EnigmaApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(EnigmaApplication.class.getResource("enigma-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/example/enigma_machine/EnigmaController.java
+++ src/main/java/com/example/enigma_machine/EnigmaController.java
@@ -1,14 +1,24 @@
 package com.example.enigma_machine;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 
 public class EnigmaController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public TextArea inputtxt;
+    @FXML
+    public TextArea outputtxt;
+    @FXML
+    public Button codificabtn;
+
+    public void onCodificaBtnClick() {
+        EnigmaMachine enigma = new EnigmaMachine(1, 2, 3, 0, 0, 0, 'A', 'A', 'A');
+        String input = inputtxt.getText();
+        String output = enigma.codificaFrase(input);
+        outputtxt.setText(output);
     }
 }
