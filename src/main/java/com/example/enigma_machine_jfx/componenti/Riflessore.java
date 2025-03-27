07cea9e412db1e25f60e1a0673c5868c0c464378
Grassi Francesco <francescograssy@proton.me>
2025-03-27 21:48:21 +0100

    Update Riflessore.java

--- src/main/java/com/example/enigma_machine_jfx/componenti/Riflessore.java
+++ src/main/java/com/example/enigma_machine_jfx/componenti/Riflessore.java
@@ -1,6 +1,22 @@
 package com.example.enigma_machine_jfx.componenti;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Riflessore {
-    //attributi: mappa caratteri del riflessore
-    //metodi: costruttore, codifica
+    private HashMap<Character, Character> caratteri;
+
+    public Riflessore(int num) {
+        ArrayList<Character> temp = UploadConfigurazioni.getRiflessore(num);
+        this.caratteri = new HashMap<>();
+        int i = 0;
+        for (Character ch : temp) {
+            this.caratteri.put((char)('A' + i), ch);
+            i++;
+        }
+    }
+
+    public Character codifica(Character lettera) {
+        return this.caratteri.get(lettera);
+    }
 }
