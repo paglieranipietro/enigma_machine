676e41acdeccfb4615d5736605a5982be569097e
P4gl14 <paglieranipietro@gmail.com>
2025-03-27 15:31:08 +0100

    indicazioni classe rotore

--- src/main/java/com/example/enigma_machine_jfx/componenti/Rotore.java
+++ src/main/java/com/example/enigma_machine_jfx/componenti/Rotore.java
@@ -1,4 +1,6 @@
 package com.example.enigma_machine_jfx.componenti;
 
 public class Rotore {
+    //attributi: ArrayList per i caratteri del rotore, posizione corrente del rotore, scatto del rotore, configurazione del rotore
+    //metodi: costruttore, letteraAvanti, letteraIndietro, ruotaAvanti, ruotaIndietro, setPosizione, getPosizione, getConfigurazione, isScatto
 }
