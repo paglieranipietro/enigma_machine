75e7e33fa18398db75eac7514937561b73bf3c52
AminataDiame <aminatadiame123@gmail.com>
2025-03-27 19:12:37 +0100

    Classe rotore con costruttore,setPosizione,getPosizione e getConfigurazione

--- src/main/java/com/example/enigma_machine_jfx/componenti/Rotore.java
+++ src/main/java/com/example/enigma_machine_jfx/componenti/Rotore.java
@@ -1,6 +1,33 @@
 package com.example.enigma_machine_jfx.componenti;
 
+import java.util.ArrayList;
+
 public class Rotore {
     //attributi: ArrayList per i caratteri del rotore, posizione corrente del rotore, scatto del rotore, configurazione del rotore
     //metodi: costruttore, letteraAvanti, letteraIndietro, ruotaAvanti, ruotaIndietro, setPosizione, getPosizione, getConfigurazione, isScatto
+    private ArrayList<Character> caratteri;
+    int posizione;
+    int scatto;
+    int configurazione;
+
+    public Rotore(int configurazione, int posizione) {
+        this.configurazione = configurazione;
+        this.caratteri = UploadConfigurazioni.getRotore(configurazione);
+        this.posizione = posizione;
+        this.scatto = UploadConfigurazioni.getAlfabeto().indexOf(UploadConfigurazioni.getScatto(configurazione));
+    }
+
+    public void setPosizione(int posizione) {this.posizione = posizione;}
+
+    public int getPosizione() {return posizione;}
+
+    public int getConfigurazione() {return configurazione;}
+
+    public boolean isScatto(boolean avanti) {
+        if (avanti){
+            return posizione == scatto;
+        }else{
+            return (posizione - 1 + 26) % 26 == scatto;
+        }
+    }
 }
