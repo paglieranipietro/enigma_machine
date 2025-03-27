6ca927a7401527957206e999bdf8eae0394f048a
AminataDiame <aminatadiame123@gmail.com>
2025-03-27 19:53:20 +0100

    letteraAvanti,letteraIndietro,ruotaAvanti,ruotaIndietro

--- src/main/java/com/example/enigma_machine_jfx/componenti/Rotore.java
+++ src/main/java/com/example/enigma_machine_jfx/componenti/Rotore.java
@@ -16,6 +16,30 @@ public class Rotore {
         this.posizione = posizione;
         this.scatto = UploadConfigurazioni.getAlfabeto().indexOf(UploadConfigurazioni.getScatto(configurazione));
     }
+    public char letteraAvanti (char c) {
+        char carattere = caratteri.get((c - 'A' + posizione) % 26);
+        return (char) ((carattere - 'A' - posizione + 26) % 26 + 'A');
+    }
+
+    public char letteraIndietro (char c) {
+        char carattere = (char) (caratteri.indexOf((char) ((c - 'A' + posizione) % 26 + 'A')) + 'A');
+        return (char) ((carattere - 'A' - posizione + 26) % 26 + 'A');
+    }
+    public boolean ruotaAvanti(int PC) {
+        if (PC == 0 && (posizione % 26) == 25) {
+            return false;
+        }
+        posizione = (posizione + 1) % 26;
+        return posizione == ((scatto + 1) % 26);
+    }
+
+    public boolean ruotaIndietro(int programCounter) {
+        if (programCounter == 0 && posizione == 0) {
+            return false;
+        }
+        posizione = (posizione - 1 + 26) % 26;
+        return posizione == scatto;
+    }
 
     public void setPosizione(int posizione) {this.posizione = posizione;}
 
