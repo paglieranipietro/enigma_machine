14f73159b712e276530615d7560ccb171b2bdc8e
P4gl14 <paglieranipietro@gmail.com>
2025-03-27 21:35:31 +0100

    indicazioni classe Lampadina

--- src/main/java/com/example/enigma_machine_jfx/componenti/Lampadina.java
+++ src/main/java/com/example/enigma_machine_jfx/componenti/Lampadina.java
@@ -1,4 +1,8 @@
 package com.example.enigma_machine_jfx.componenti;
 
 public class Lampadina {
+    //Realizza una "lampadina" attraverso StackPane e le animazioni di TimeLine
+    //se spenta sarà grigia e se accesa sarà gialla, in entrambi i casi si legge la lettera corrispondente in sovraimpressione
+    //di dimensione 25 e font 18
+
 }
