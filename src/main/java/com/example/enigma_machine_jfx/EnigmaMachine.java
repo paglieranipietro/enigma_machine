58cb17039dac039871fa51937c2050a5209ae43a
P4gl14 <paglieranipietro@gmail.com>
2025-03-28 11:39:03 +0100

    gestione grafica dei rotori

--- src/main/java/com/example/enigma_machine_jfx/EnigmaMachine.java
+++ src/main/java/com/example/enigma_machine_jfx/EnigmaMachine.java
@@ -6,7 +6,6 @@ import com.example.enigma_machine_jfx.componenti.Rotore;
 import javafx.scene.control.TextField;
 
 public class EnigmaMachine {
-    //gestione della vera e propria macchina enigma con la codifica delle lettere
     private Rotore rotoreDestro;
     private Rotore rotoreCentrale;
     private Rotore rotoreSinistro;
@@ -26,6 +25,14 @@ public class EnigmaMachine {
         this.posInizialesx = 0;
         this.riflessore = new Riflessore(confRiflessore);
         this.pannelloScambiatore = new PannelloScambiatore();
+    }
+
+    private void incrementaPC(boolean bottoni){
+        if (!bottoni)PC++;
+    }
+
+    private void decrementaPC(){
+        if (PC > 0) PC--;
     }
 
     public char codificaCarattere(char c) {
@@ -83,6 +90,100 @@ public class EnigmaMachine {
         PC = 0;
     }
 
+    public void ruotaRotoreDestro(boolean avanti, boolean iniziale) {
+        if (avanti){
+            incrementaPC(true);
+            rotoreDestro.ruotaAvanti(PC);
+        }else {
+            decrementaPC();
+            rotoreDestro.ruotaIndietro(PC);
+        }
+        if (iniziale) posInizialedx = rotoreDestro.getPosizione();
+    }
+
+    public void ruotaRotoreCentrale(boolean avanti, boolean iniziale) {
+        if (avanti){
+            incrementaPC(true);
+            rotoreCentrale.ruotaAvanti(PC);
+        }else {
+            decrementaPC();
+            rotoreCentrale.ruotaIndietro(PC);
+        }
+        if (iniziale) posInizialecn = rotoreCentrale.getPosizione();
+    }
+
+    public void ruotaRotoreSinistro(boolean avanti, boolean iniziale) {
+        if (avanti){
+            incrementaPC(true);
+            rotoreSinistro.ruotaAvanti(PC);
+        }else {
+            decrementaPC();
+            rotoreSinistro.ruotaIndietro(PC);
+        }
+        if (iniziale) posInizialesx = rotoreSinistro.getPosizione();
+    }
+
+    public int getPosizioneRotoreDestro() {
+        return rotoreDestro.getPosizione();
+    }
+
+    public int getPosizioneRotoreCentrale() {
+        return rotoreCentrale.getPosizione();
+    }
+
+    public int getPosizioneRotoreSinistro() {
+        return rotoreSinistro.getPosizione();
+    }
+
+    public static void aggiornaRotoreTxt(TextField txt, boolean aggiungi, int pos){
+        if (pos != -1){
+            txt.setText(String.valueOf((char)('A' + pos)));
+        }else if (aggiungi){
+            String temp = txt.getText();
+            char c = temp.charAt(0);
+            c = c >= 'Z' ? 'Z' : (char)(c + 1);
+            txt.setText(String.valueOf(c));
+        }else{
+            String temp = txt.getText();
+            char c = temp.charAt(0);
+            c = c <= 'A' ? 'A' : (char)(c - 1);
+            txt.setText(String.valueOf(c));
+        }
+    }
+
+    public String getConfRotore(String rotore) {
+        switch (rotore) {
+            case "dx":
+                switch (rotoreDestro.getConfigurazione()) {
+                    case 1:
+                        return "I";
+                    case 2:
+                        return "II";
+                    case 3:
+                        return "III";
+                }
+            case "cn":
+                switch (rotoreCentrale.getConfigurazione()) {
+                    case 1:
+                        return "I";
+                    case 2:
+                        return "II";
+                    case 3:
+                        return "III";
+                }
+            case "sx":
+                switch (rotoreSinistro.getConfigurazione()) {
+                    case 1:
+                        return "I";
+                    case 2:
+                        return "II";
+                    case 3:
+                        return "III";
+                }
+        }
+        return null;
+    }
+
     public void aggiungiScambio(char c1, char c2){
         pannelloScambiatore.aggiungiScambio(c1, c2);
     }
@@ -93,13 +194,5 @@ public class EnigmaMachine {
 
     public boolean contieneScambio(char c, char c2){
         return pannelloScambiatore.contieneScambio(c) || pannelloScambiatore.contieneScambio(c2);
-    }
-
-    private void incrementaPC(boolean bottoni){
-        if (!bottoni)PC++;
-    }
-
-    private void decrementaPC(){
-        if (PC > 0) PC--;
     }
 }
--- src/main/java/com/example/enigma_machine_jfx/EnigmaMachineController.java
+++ src/main/java/com/example/enigma_machine_jfx/EnigmaMachineController.java
@@ -26,23 +26,96 @@ public class EnigmaMachineController {
     public ComboBox rotdxcmb;
     @FXML
     public ComboBox riflcmb;
+    @FXML
+    private EnigmaMachine enigma;
 
 
-    public void onMenosxBtnClick() {
+    @FXML
+    public void initialize() {
+        inizializzaMenuRotore();
+        inizializzaEnigmaMachine();
+        aggiungiListenerComboBox();
     }
 
-    public void onPiusxBtnClick() {
+    private void inizializzaMenuRotore(){
+        riflcmb.getItems().addAll(new String[]{"A", "B", "C"});
+        riflcmb.getSelectionModel().select(1);
+        rotsxcmb.getItems().addAll(new String[]{"I", "II", "III"});
+        rotsxcmb.getSelectionModel().select(2);
+        rotcncmb.getItems().addAll(new String[]{"I", "II", "III"});
+        rotcncmb.getSelectionModel().select(1);
+        rotdxcmb.getItems().addAll(new String[]{"I", "II", "III"});
+        rotdxcmb.getSelectionModel().select(0);
     }
 
-    public void onMenocnBtnClick() {
+    private void inizializzaEnigmaMachine(){
+        enigma = new EnigmaMachine(rotdxcmb.getSelectionModel().getSelectedIndex(), rotcncmb.getSelectionModel().getSelectedIndex(), rotsxcmb.getSelectionModel().getSelectedIndex(), riflcmb.getSelectionModel().getSelectedIndex());
+        rotsxtxt.setText("" + 'A');
+        rotcntxt.setText("" + 'A');
+        rotdxtxt.setText("" + 'A');
+        rifltxt.setText("" + 'A');
     }
 
-    public void onPiucnBtnClick() {
+    private void aggiungiListenerComboBox() {
+        // Listener per riflcmb
+        riflcmb.setOnAction(event -> {
+            String nuovoValore = (String) riflcmb.getValue();
+            if (!nuovoValore.equals(enigma.getConfRotore("rif"))) {
+                inizializzaEnigmaMachine(); // Chiama la funzione se il valore è cambiato
+            }
+        });
+        // Listener per rotsxcmb
+        rotsxcmb.setOnAction(event -> {
+            String nuovoValore = (String) rotsxcmb.getValue();
+            if (!nuovoValore.equals(enigma.getConfRotore("sx"))) {
+                inizializzaEnigmaMachine(); // Chiama la funzione se il valore è cambiato
+            }
+        });
+
+        // Listener per rotcncmb
+        rotcncmb.setOnAction(event -> {
+            String nuovoValore = (String) rotcncmb.getValue();
+            if (!nuovoValore.equals(enigma.getConfRotore("cn"))) {
+                inizializzaEnigmaMachine(); // Chiama la funzione se il valore è cambiato
+            }
+        });
+
+        // Listener per rotdxcmb
+        rotdxcmb.setOnAction(event -> {
+            String nuovoValore = (String) rotdxcmb.getValue();
+            if (!nuovoValore.equals(enigma.getConfRotore("dx"))) {
+                inizializzaEnigmaMachine(); // Chiama la funzione se il valore è cambiato
+            }
+        });
     }
 
-    public void onMenodxBtnClick() {
+    public void onPiusxBtnClick(){
+        enigma.ruotaRotoreSinistro(true, true);
+        EnigmaMachine.aggiornaRotoreTxt(rotsxtxt, true, -1);
     }
 
-    public void onPiudxBtnClick() {
+    public void onMenosxBtnClick(){
+        enigma.ruotaRotoreSinistro(false, true);
+        EnigmaMachine.aggiornaRotoreTxt(rotsxtxt, false, -1);
+    }
+
+    public void onPiucnBtnClick(){
+        enigma.ruotaRotoreCentrale(true, true);
+        EnigmaMachine.aggiornaRotoreTxt(rotcntxt, true, -1);
+    }
+
+    public void onMenocnBtnClick(){
+        enigma.ruotaRotoreCentrale(false, true);
+        EnigmaMachine.aggiornaRotoreTxt(rotcntxt, false, -1);
+    }
+
+    public void onPiudxBtnClick(){
+        enigma.ruotaRotoreDestro(true, true);
+        EnigmaMachine.aggiornaRotoreTxt(rotdxtxt, true, -1);
+    }
+
+    public void onMenodxBtnClick(){
+        enigma.ruotaRotoreDestro(false, true);
+        EnigmaMachine.aggiornaRotoreTxt(rotdxtxt, false, -1);
     }
 }
