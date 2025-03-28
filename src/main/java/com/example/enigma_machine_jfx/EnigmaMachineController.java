3bf72be9f3806d671cdfa140688f7922b257a855
P4gl14 <paglieranipietro@gmail.com>
2025-03-28 13:43:41 +0100

    listener TextAreas

--- src/main/java/com/example/enigma_machine_jfx/EnigmaMachineController.java
+++ src/main/java/com/example/enigma_machine_jfx/EnigmaMachineController.java
@@ -34,6 +34,7 @@ public class EnigmaMachineController {
     public void initialize() {
         inizializzaMenuRotore();
         inizializzaEnigmaMachine();
+        inizializzazioneListenerInputtxt();
         aggiungiListenerComboBox();
     }
 
@@ -54,6 +55,101 @@ public class EnigmaMachineController {
         rotcntxt.setText("" + 'A');
         rotdxtxt.setText("" + 'A');
         rifltxt.setText("" + 'A');
+    }
+
+    public void onPiusxBtnClick(){
+        enigma.ruotaRotoreSinistro(true, true);
+        EnigmaMachine.aggiornaRotoreTxt(rotsxtxt, true, -1);
+    }
+
+    public void onMenosxBtnClick(){
+        enigma.ruotaRotoreSinistro(false, true);
+        EnigmaMachine.aggiornaRotoreTxt(rotsxtxt, false, -1);
+    }
+
+    public void onPiucnBtnClick(){
+        enigma.ruotaRotoreCentrale(true, true);
+        EnigmaMachine.aggiornaRotoreTxt(rotcntxt, true, -1);
+    }
+
+    public void onMenocnBtnClick(){
+        enigma.ruotaRotoreCentrale(false, true);
+        EnigmaMachine.aggiornaRotoreTxt(rotcntxt, false, -1);
+    }
+
+    public void onPiudxBtnClick(){
+        enigma.ruotaRotoreDestro(true, true);
+        EnigmaMachine.aggiornaRotoreTxt(rotdxtxt, true, -1);
+    }
+
+    public void onMenodxBtnClick(){
+        enigma.ruotaRotoreDestro(false, true);
+        EnigmaMachine.aggiornaRotoreTxt(rotdxtxt, false, -1);
+    }
+
+    private void inizializzazioneListenerInputtxt() {
+        inputtxt.textProperty().addListener((observable, oldValue, newValue) -> {
+            // Calcola la differenza di lunghezza tra il nuovo valore e il vecchio valore
+            int differenza = newValue.length() - oldValue.length();
+
+            if (differenza > 0) {
+                // Nuovi caratteri inseriti
+                for (int i = oldValue.length(); i < newValue.length(); i++) {
+                    char newChar = newValue.charAt(i);
+                    if (okCarattere(newChar)) {
+                        // Codifica il carattere valido e aggiungilo a outputtxt
+                        char transformedChar = enigma.codificaCarattere(newChar);
+                        if (transformedChar != '!') {
+                            outputtxt.appendText(String.valueOf(transformedChar));
+
+                            // Aggiungi uno spazio ogni 5 caratteri (escludendo gli spazi)
+                            String outputTextWithoutSpaces = outputtxt.getText().replace(" ", "");
+                            if (outputTextWithoutSpaces.length() % 5 == 0) {
+                                outputtxt.appendText(" ");
+                            }
+                        }
+                    }
+                }
+            } else if (differenza < 0) {
+                // Caratteri rimossi (backspace o cancellazione)
+                int numCaratteriRimossi = oldValue.length() - newValue.length();
+
+                // Verifica se i caratteri rimossi erano validi
+                for (int i = oldValue.length() - 1; i >= newValue.length(); i--) {
+                    char removedChar = oldValue.charAt(i);
+                    if (okCarattere(removedChar)) {
+                        // Ruota i rotori all'indietro per ogni carattere valido rimosso
+                        enigma.codificaCarattere('\b');
+
+                        // Rimuovi l'ultimo carattere valido da outputtxt (ignorando gli spazi)
+                        String currentOutput = outputtxt.getText();
+                        if (currentOutput.length() > 0) {
+                            // Trova l'ultimo carattere valido (non spazio)
+                            int lastValidCharIndex = currentOutput.length() - 1;
+                            while (lastValidCharIndex >= 0 && currentOutput.charAt(lastValidCharIndex) == ' ') {
+                                lastValidCharIndex--;
+                            }
+
+                            // Se è stato trovato un carattere valido, rimuovilo
+                            if (lastValidCharIndex >= 0) {
+                                outputtxt.setText(currentOutput.substring(0, lastValidCharIndex));
+                            } else {
+                                outputtxt.setText(""); // Se non ci sono caratteri validi, svuota outputtxt
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Aggiorna le visualizzazioni dei rotori
+            EnigmaMachine.aggiornaRotoreTxt(rotsxtxt, true, enigma.getPosizioneRotoreSinistro());
+            EnigmaMachine.aggiornaRotoreTxt(rotcntxt, true, enigma.getPosizioneRotoreCentrale());
+            EnigmaMachine.aggiornaRotoreTxt(rotdxtxt, true, enigma.getPosizioneRotoreDestro());
+        });
+    }
+
+    private boolean okCarattere(char c) {
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
     }
 
     private void aggiungiListenerComboBox() {
@@ -87,35 +183,5 @@ public class EnigmaMachineController {
                 inizializzaEnigmaMachine(); // Chiama la funzione se il valore è cambiato
             }
         });
-    }
-
-    public void onPiusxBtnClick(){
-        enigma.ruotaRotoreSinistro(true, true);
-        EnigmaMachine.aggiornaRotoreTxt(rotsxtxt, true, -1);
-    }
-
-    public void onMenosxBtnClick(){
-        enigma.ruotaRotoreSinistro(false, true);
-        EnigmaMachine.aggiornaRotoreTxt(rotsxtxt, false, -1);
-    }
-
-    public void onPiucnBtnClick(){
-        enigma.ruotaRotoreCentrale(true, true);
-        EnigmaMachine.aggiornaRotoreTxt(rotcntxt, true, -1);
-    }
-
-    public void onMenocnBtnClick(){
-        enigma.ruotaRotoreCentrale(false, true);
-        EnigmaMachine.aggiornaRotoreTxt(rotcntxt, false, -1);
-    }
-
-    public void onPiudxBtnClick(){
-        enigma.ruotaRotoreDestro(true, true);
-        EnigmaMachine.aggiornaRotoreTxt(rotdxtxt, true, -1);
-    }
-
-    public void onMenodxBtnClick(){
-        enigma.ruotaRotoreDestro(false, true);
-        EnigmaMachine.aggiornaRotoreTxt(rotdxtxt, false, -1);
     }
 }
